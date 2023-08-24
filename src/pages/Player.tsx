5094d395807775ab09177e08f51465c5d3d371c4
Ericlys Moreira <ericlysm@gmail.com>
2023-08-24 12:51:57 -0300

    chore: zustand setup

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -41,7 +41,7 @@ export function Player() {
           <div className="flex-1">
             <Video/>
           </div>
-          <aside className="w-80 absolute top-0 bottom-0 right-0 border-l divide-y-2 divide-zinc-900 border-zinc-800 bg-zinc-900 overflow-y-scroll scrollbar scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-800 ">
+          <aside className="w-80 absolute top-0 bottom-0 right-0 border-l divide-y-2 divide-zinc-900 border-zinc-800 bg-zinc-900 overflow-y-scroll scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-800 ">
             {
               modules && modules.map((module, index) => {
                 return (
