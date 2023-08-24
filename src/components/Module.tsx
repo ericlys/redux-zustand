1edc4ecb5f495327a9dd9adf66f9536a7d3daedb
Ericlys Moreira <ericlysm@gmail.com>
2023-08-23 22:02:53 -0300

    chore: highlighting current class

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -15,6 +15,12 @@ interface ModuleProps {
 export function Module({moduleIndex, title, amountOfLessons}: ModuleProps) {
   const dispatch = useDispatch()
 
+  const { currentLessonIndex, currentModuleIndex } = useAppSelector(state => {
+    const { currentModuleIndex, currentLessonIndex } = state.player
+
+    return {currentLessonIndex, currentModuleIndex}
+  })
+
   const lessons = useAppSelector(state => {
     return state.player.course.modules[moduleIndex].lessons
   })
@@ -36,13 +42,18 @@ export function Module({moduleIndex, title, amountOfLessons}: ModuleProps) {
 
       <Collapsible.Content>
         <nav className="relative flex flex-col gap-4 p-6">
-          {lessons.map( (lesson, lessonIndex) => 
-            <Lesson 
-              key={lesson.id} 
-              title={lesson.title} 
-              duration={lesson.duration}
-              onPlay={() => dispatch(play([moduleIndex, lessonIndex]))}
-            />
+          {lessons.map( (lesson, lessonIndex) => {
+            const isCurrent = currentModuleIndex === moduleIndex && currentLessonIndex === lessonIndex
+            return (
+              <Lesson 
+                key={lesson.id} 
+                title={lesson.title} 
+                duration={lesson.duration}
+                onPlay={() => dispatch(play([moduleIndex, lessonIndex]))}
+                isCurrent={isCurrent}
+              />
+            )
+          }
           )}
         </nav>
       </Collapsible.Content>
